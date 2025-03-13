62e00db364af7141c7b1eb6a0d475e11ea428545
SerGit <sergey-net.com86@mail.ru>
2025-03-13 18:36:23 +0300

    project m1 first commit

--- src/org/javaacadmey/wonder_field/Game.java
+++ src/org/javaacadmey/wonder_field/Game.java
@@ -1,4 +1,20 @@
 package org.javaacadmey.wonder_field;
 
+import java.util.Scanner;
+
 public class Game {
+    private final int amountPlayers = 3;
+    private final int amountRounds = 4;
+    private final int groupRounds = 3;
+    private final int indexFinalRounds = 3;
+
+    Scanner scanner = new Scanner(System.in);
+
+    private void init() {
+        System.out.println(
+                "Запуск игры \"Поле Чудес\" - подготовка к игре. " + "Вам нужно ввести вопросы и ответы для игры.");
+
+        System.out.println("Введите вопрос #1");
+        String quest1 = scanner.nextLine();
+    }
 }
--- src/org/javaacadmey/wonder_field/Runner.java
+++ src/org/javaacadmey/wonder_field/Runner.java
@@ -1,4 +1,8 @@
 package org.javaacadmey.wonder_field;
 
 public class Runner {
+
+    public static void main(String[] args) {
+
+    }
 }
